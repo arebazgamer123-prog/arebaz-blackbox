b8543f52f2af00e81644c3ee4111a668e01146ab
arebazgamer123-prog <arebazgamer123@gmail.com>
2025-12-15 17:40:54 +0530

    Create menu.py

---
+++ ui/menu.py
@@ -0,0 +1,7 @@
+def menu():
+    print("\nSelect Scan Mode:")
+    print("[1] Quick Scan")
+    print("[2] Full Recon Scan")
+    print("[3] Web Scan")
+    print("[4] SSL Scan")
+    return input("Enter option: ")
