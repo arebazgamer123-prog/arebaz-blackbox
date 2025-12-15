13a4f8f6ae3b0c8c85fd922a70c45afdffb2868b
arebazgamer123-prog <arebazgamer123@gmail.com>
2025-12-15 17:33:48 +0530

    Add initial print statements and user input for target

---
+++ main.py
@@ -0,0 +1,8 @@
+print("""
+🕶️ AREBAZ BLACKBOX
+Advanced Network Reconnaissance Framework
+-----------------------------------------
+""")
+
+target = input("Enter Target (IP / Domain): ")
+print("Target selected:", target)
